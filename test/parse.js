73a81576005e610dea0dc496344231bd0acc3c54
pcty-kk <jxk0901@126.com>
2023-04-23 10:31:10 +0800

    feat: parse + to space

--- test/parse.js
+++ test/parse.js
@@ -4,6 +4,7 @@ var qs = require("../lib");
 test("parse()", function (t) {
   t.test("parse a simple string", function (st) {
     st.deepEqual(qs.parse("0=foo"), { 0: "foo" });
+    st.deepEqual(qs.parse("foo=c++"), { foo: "c  " });
     st.end();
   });
 });
