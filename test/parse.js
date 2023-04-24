6c3b6c459f026100579e70d2a0ca0e01eb503710
pcty-kk <jxk0901@126.com>
2023-04-24 22:00:45 +0800

    feat: 解析多层对象语法

--- test/parse.js
+++ test/parse.js
@@ -5,6 +5,9 @@ test("parse()", function (t) {
   t.test("parse a simple string", function (st) {
     st.deepEqual(qs.parse("0=foo"), { 0: "foo" });
     st.deepEqual(qs.parse("foo=c++"), { foo: "c  " });
+    st.deepEqual(qs.parse("a[>=]=23"), { a: { ">=": "23" } });
+    st.deepEqual(qs.parse("a[<=>]==23"), { a: { "<=>": "=23" } });
+    st.deepEqual(qs.parse("a[==]=23"), { a: { "==": "23" } });
     st.end();
   });
 });
